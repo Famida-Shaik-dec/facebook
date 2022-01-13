0a898f2bfb102b18f7e5d46a978de58b74df26b0
FamidaShaik <famidashaik494@gmail.com>
2022-01-13 22:30:40 +0530

    added git ignore file

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
-I am updating  in master branch 
+
+i am updating  in master branch 
 this file deployment logic..
+=======
+ HI..am upadting this in dev branch 
+ this file deployment logic..
 updating line
